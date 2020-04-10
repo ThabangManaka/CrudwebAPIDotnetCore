fcb29994c6aa2d4b410d20e02d16f0f65c2dcd05
thabangdansteyn <thabangdansteyn@gmail.com>
2020-04-10 23:38:30 +0200

    Crud API with Swagger.

---
+++ CrudOperationWebAPI/CrudOperationWebAPI/Data/CrudOperationWebAPIContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperationWebAPI.Models
+{
+    public class CrudOperationWebAPIContext : DbContext
+    {
+        public CrudOperationWebAPIContext (DbContextOptions<CrudOperationWebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CrudOperationWebAPI.Models.User> User { get; set; }
+    }
+}
